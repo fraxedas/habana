47b9155ab99bb4bcef4079ded07263d49611ba64
Oscar Fraxedas <oscar.fraxedas.tormo@oracle.com>
2015-09-08 16:57:23 -0400

    Improving middleware

--- filters/authentication/index.js
+++ filters/authentication/index.js
@@ -5,40 +5,45 @@
 
 	authentication.init = function (app) {
 
-		app.all("/auth/*", function (req, res, next) {
-			console.log("Intercepting requests for login");
+		var bypass = function (req, res, next) {
 			req.noauth = true;
 			next();
-		});
+		};
 
-		app.all("/oauth/*", function (req, res, next) {
-			console.log("Intercepting requests for oauth");
-			req.noauth = true;
-			next();
-		});
-
-		app.all("/*", function (req, res, next) {
-			console.log("Intercepting requests for everything else");
-
-			var user = cookies.get(req);
+		var authenticate = function (req, res, next) {
 			
-			if(req.noauth) next();			
-			
-			if (user) {
-				data.get_session(user.session_id, function (err, session) {
-					if (err) {
-						res.redirect("/auth/signin");
-					}
-					else {
-						req.session = session;
-						next();
-					}
-				});
+			if (req.noauth) {
+				next();
 			}
 			else {
-				res.redirect("/auth/signin");
-			}
+				var user = cookies.get(req);
 
+				if (user) {
+					data.get_session(user.session_id, function (err, session) {
+						if (err) {
+							res.redirect("/auth/signin");
+						}
+						else {
+							req.session = session;
+							next();
+						}
+					});
+				}
+				else {
+					res.redirect("/auth/signin");
+				}
+			}
+		};
+
+		app.all("/auth/*", bypass);
+
+		app.all("/oauth/*", bypass);
+
+		app.all("/*", authenticate);
+
+		app.use(function (err, req, res, next) {
+			console.error(err.stack);
+			res.render("error", { error: 'Something failed', body: err });
 		});
 
 	};
