1bb8ba38e59da24a4ed3102192eca905d240e113
Oscar Fraxedas <fraxedas@gmail.com>
2016-01-27 17:27:17 -0500

    google oauth fixes

--- controllers/google/oauth.js
+++ controllers/google/oauth.js
@@ -1,42 +1,46 @@
 (function (oauth) {
 
-	var persist = require('node-persist');
-    persist.initSync();
-	var google_api = require('../../lib/google-api');
+	var data = require('../../lib/data');
+    var google_api = require('../../lib/google-api');
 	var session = require("../../lib/session");
 
 	oauth.init = function (app) {
 
 		app.get('/oauth/google/auth', function (req, res) {
-			var google = persist.getItem('google');
-			var redirect_url = redirect(req);
-			var url = google_api.get_auth_url(google.client_id, google.client_secret, redirect_url);
-			res.redirect(url);
+            data.get_provider('google', function (err, google) {
+                if (err) {
+                    res.render("error", { error: 'Something failed while authenticating with google', body: err });
+                } else {
+                    var redirect_url = redirect(req);
+                    var url = google_api.get_auth_url(google.client_id, google.client_secret, redirect_url);
+                    res.redirect(url);
+                }
+            });			
 		});
 
 		app.get('/oauth/google/callback', function (req, res) {
 			var code = req.query.code;
-			var google = persist.getItem('google');
 			var redirect_url = redirect(req);
 
-			google_api.get_tokens(google.client_id, google.client_secret, redirect_url, code, function (err, tokens) {
-				if (err) {
-					res.render("error", { error: 'Something failed while authenticating with google', body: err });
-				}
-				else {
-					google.tokens = tokens;
-					persist.setItem('google', google);
-					session.set(res, google, function (err) {
-						if (err) {
-							res.render("error", { error: 'Something failed while authenticating with google', body: err });
-						}
-						else {
-							res.render("google/google", { title: 'Google oauth is done' });
-						}
-					});
+            data.get_provider('google', function (err, google) {
+                google_api.get_tokens(google.client_id, google.client_secret, redirect_url, code, function (err, tokens) {
+                    if (err) {
+                        res.render("error", { error: 'Something failed while authenticating with google', body: err });
+                    }
+                    else {
+                        google.tokens = tokens;
+                        session.set(res, google, function (err) {
+                            if (err) {
+                                res.render("error", { error: 'Something failed while authenticating with google', body: err });
+                            }
+                            else {
+                                res.render("google/google", { title: 'Google oauth is done' });
+                            }
+                        });
 
-				}
-			});
+                    }
+                });
+            });	
 		});
 
 		var redirect = function (req) {
