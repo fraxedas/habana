5f8b5984b9dc9276b0e1ebe7032ecb88dcffa098
Oscar Fraxedas <fraxedas@gmail.com>
2016-04-13 16:06:10 -0400

    Some cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,13 +8,32 @@ module.exports = function(grunt) {
         watch: {
             files: files,
             tasks : ['jshint']
-        }
+        },
+        coveralls: {
+            // Options relevant to all targets
+            options: {
+                // When true, grunt-coveralls will only print a warning rather than
+                // an error, to prevent CI builds from failing unnecessarily (e.g. if
+                // coveralls.io is down). Optional, defaults to false.
+                force: false
+            },
+
+            habana: {
+                // LCOV coverage file (can be string, glob or array)
+                src: './coverage/*.info',
+                options: {
+                    // Any options for just this target
+                }
+            },
+  }
     });
 
     // Load the plugin that provides the "jshint" task.
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
     grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.loadNpmTasks('grunt-coveralls');
 
     // Default task(s).
     grunt.registerTask('default', ['jshint']);
